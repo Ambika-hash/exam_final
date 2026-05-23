09753dde5d90aa3e5ed4ebe993e61f2fec2ef5aa
Ambika <ambika.m.sharma2005@gmail.com>
2026-05-23 09:42:08 +0530

    Update Evenodd.java

--- evenodd/src/evenodd/Evenodd.java
+++ evenodd/src/evenodd/Evenodd.java
@@ -7,7 +7,7 @@ public class Evenodd {
 		Scanner s = new Scanner(System.in);
 		int input = s.nextInt();
 		if (input % 2 == 0) {
-			System.out.print("is even number");
+			System.out.print("is even number123333");
 		}else {
 			System.out.print("is Oufjgdd number");
 		}
